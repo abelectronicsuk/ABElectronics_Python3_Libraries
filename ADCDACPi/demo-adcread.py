4fe4e0056b1151451ed9dfeac39f1fa943e8d787
Andrew Dorey <sales@abelectronics.co.uk>
2016-06-24 21:32:35 +0100

    Updated read_adc_voltage() and read_adc_raw()
    Updated the adc read functions to allow single ended or differential
    mode.  You can now specify the mode with an additional parameter when
    reading a voltage.

--- ADCDACPi/demo-adcread.py
+++ ADCDACPi/demo-adcread.py
@@ -21,6 +21,6 @@ adcdac = ADCDACPi()  # create an instance of ADCDACPi
 adcdac.set_adc_refvoltage(3.3)
 
 while True:
-    # read the voltage from channel 1 and display on the screen
-    print (adcdac.read_adc_voltage(1))
+    # read the voltage from channel 1 in single ended mode and display on the screen
+    print (adcdac.read_adc_voltage(1, 0))
     time.sleep(0.5)
